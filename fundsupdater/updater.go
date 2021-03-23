969c48957437c413fc249e82d1deebd91b171dfb
John Bayko <jbayko@sasktel.net>
2021-03-23 16:57:11 -0600

    Parse load amount properly.
    - Moved get transaction code to new function.
    - Added parsing for load amount string to get cents.

--- fundsupdater/updater.go
+++ fundsupdater/updater.go
@@ -5,14 +5,76 @@ import (
     "fmt"
     "io"
     "os"
+    "regexp"
+    "strconv"
     "time"
 )
 
 type Transaction struct {
-    Id string `json:"id"`
-    Customer_id string `json:"customer_id"`
-    Load_amount string `json:"load_amount"`
-    Time time.Time `json:"time"`
+    Id string
+    Customer_id string
+
+    // Input amount is no more than 8 characters, or 6 digits.
+    // n digits take roughly n * log2(10) = 6 * 3.3 bits = 20,
+    // so 64 is plenty in this case.
+    Load_amount_cents int64
+
+    Time time.Time
+}
+
+var (
+    amount_re = regexp.MustCompile(`\$(\d+).(\d{2})`)
+)
+
+/*
+    Get the next transaction from the given json.Decoder, and fill the given
+    Transaction fields.
+
+    Returns boolean to indicate end of transactions, and error for current
+    transaction.
+ */
+func get_transaction(input_decoder *json.Decoder, t *Transaction) (bool, error) {
+    type JsonTransaction struct {
+        Id string `json:"id"`
+        Customer_id string `json:"customer_id"`
+        Load_amount string `json:"load_amount"`
+        Time time.Time `json:"time"`
+    }
+    var jt JsonTransaction
+
+    decode_err := input_decoder.Decode(&jt)
+    if decode_err != nil {
+        if decode_err == io.EOF {
+            return true, nil
+        } else {
+            fmt.Fprintf(os.Stderr, "Decode input: %v\n", decode_err)
+            return false, decode_err
+        }
+    }
+    t.Id = jt.Id
+    t.Customer_id = jt.Customer_id
+
+    // Decode string to cents `$([\d]+).([\d]{2})`
+    amount_match := amount_re.FindStringSubmatch(jt.Load_amount)
+    if len(amount_match) != 3 {
+        return false, fmt.Errorf(
+            "Amount not a valid \"$dollar.cents\" string: %v", jt.Load_amount)
+    }
+
+    // 2 digits for cents is about 7 bits, so allow no more than 64-7 = 57 bits
+    // for dollars.
+    amount_dollars, dollars_err := strconv.ParseInt(amount_match[1], 10, 57)
+    if dollars_err != nil {
+        return false, dollars_err
+    }
+    // The regex ensures this is 2 decimal digits, so parse will succeed,
+    // error can be ignored.
+    amount_cents, _ := strconv.ParseInt(amount_match[2], 10, 7)
+
+    t.Load_amount_cents = (amount_dollars * 100) + amount_cents
+    t.Time = jt.Time
+
+    return false, nil
 }
 
 func update(input_file_name string, output_file_name string) error {
@@ -35,20 +97,19 @@ func update(input_file_name string, output_file_name string) error {
 
     input_decoder := json.NewDecoder(input_file)
     for {
-        decode_err := input_decoder.Decode(&t)
+        is_end, decode_err := get_transaction(input_decoder, &t)
+        if is_end {
+            return nil
+        }
         if decode_err != nil {
-            if decode_err == io.EOF {
-                return nil
-            } else {
-                fmt.Fprintf(os.Stderr, "Decode input: %v\n", decode_err)
-                return decode_err
-            }
+            fmt.Fprintf(os.Stderr, "Decode input: %v\n", decode_err)
+            continue  // Try next transaction
         }
         fmt.Printf(
             "id %v customer_id %v load_amount %v time %v\n",
             t.Id,
             t.Customer_id,
-            t.Load_amount,
+            t.Load_amount_cents,
             t.Time)  // debug
 
         // Handle transaction and output error if any
