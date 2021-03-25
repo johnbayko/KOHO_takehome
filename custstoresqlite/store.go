5a5d48c8d4b49eb7781eb4a3050644d375697711
John Bayko <jbayko@sasktel.net>
2021-03-24 19:57:25 -0600

    Check for existing transaction before processing

--- custstoresqlite/store.go
+++ custstoresqlite/store.go
@@ -2,6 +2,7 @@ package custstoresqlite
 
 import (
     "database/sql"
+    "fmt"  // debug
     "time"
 
     _ "github.com/mattn/go-sqlite3"
@@ -9,6 +10,8 @@ import (
 
 type CustStoreSqlite struct {
     db *sql.DB
+
+    checkTransId *sql.Stmt
 }
 
 func NewCustStoreSqlite() *CustStoreSqlite {
@@ -21,20 +24,42 @@ func (cs *CustStoreSqlite) Open() error {
         return err
     }
     cs.db = db
+
+    checkTransId, err := db.Prepare("select id from transactions where id = ?")
+    if err != nil {
+        db.Close()
+        return err
+    }
+    cs.checkTransId = checkTransId
+
     return nil
 }
 
 func (cs *CustStoreSqlite) Close() {
+    cs.checkTransId.Close()
     cs.db.Close()
 }
 
 /*
-    Update customer balance, save transaction if successful
+    Update apply a transaction to customer balance, save transaction record if
+    successful. Reject duplicate transaction ids.
 
     Returns error if fails.
  */
 func (cs *CustStoreSqlite) BalanceAdd(
     id string, customerId string, loadAmountCents int64, time time.Time,
 ) error {
+    // Check transaction id
+    transIdRows, err := cs.checkTransId.Query(id)
+    if err != nil {
+        return err
+    }
+    if transIdRows.Next() {
+        // Transaciton ID already there, don't insert again (but not an error)
+        return nil
+    }
+    // Update customers
+    // Update accounts
+    // Add to transactions
     return nil
 }
